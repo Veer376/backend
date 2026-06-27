02187cf42543cb73ac6171ec7825f9cabca9caee
Aryaveer <aryaveer1214@gmail.com>
2026-06-27 12:59:08 +0530

    add groq streaming

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,6 +1,6 @@
 import router from 'express';
 import { getGeminiResponse } from '../services/gemini.ts';
-import { getGroqResponse } from '../services/groq.ts';
+import { getGroqResponse, getGroqStreamResponse } from '../services/groq.ts';
 
 const chatRouter = router();
 
@@ -19,6 +19,35 @@ chatRouter.post('/chat', async (req, res) => {
     } catch (error) {
         console.log("Got the error", error);
         return res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
+chatRouter.post('/chat-stream', async (req, res) => {
+    // Set Headers for SEE.
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+
+    try {
+        const { messages, model } = req.body;
+
+        if (!messages || messages.length === 0) {
+            return res.status(400).json({ error: 'messages is required' });
+        }
+
+        const send = (data) => {
+            res.write(`data: ${JSON.stringify(data)}\n\n`);
+        };
+
+    await getGroqStreamResponse(messages, model, send);
+
+    } catch (error) {
+        console.log("Got the error", error);
+        res.write(`data: ${JSON.stringify({ error: 'Internal server error' })}\n\n`);
+
+    } finally {
+        res.end();
     }
 })
 
--- src/services/groq.ts
+++ src/services/groq.ts
@@ -24,3 +24,26 @@ export async function getGroqResponse(messages: any, model: string): Promise<any
 
   return content;
 }
+
+export async function getGroqStreamResponse(messages: any, model: string, onMessage: (data: string) => void) {
+  const contents = []
+
+  for (const message of messages) {
+    contents.push({
+      role: message.role,
+      content: message.content
+    })
+  }
+
+  const stream = await groq.chat.completions.create({
+    messages: contents,
+    model: model,
+    stream: true,
+  });
+
+  for await (const event of stream) {
+    console.log("Groq Stream Event:", event);
+    onMessage(event.choices[0]?.delta?.content || "");
+  }
+
+}
